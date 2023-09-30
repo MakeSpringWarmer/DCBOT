3fe26347a0f8df02332de971796d77fa864ff654
MakeSpringWarmer <82429989+MakeSpringWarmer@users.noreply.github.com>
2023-09-30 11:07:42 +0000

    新增cpbl_now 取得目前比分

--- Cog/cpbl.py
+++ Cog/cpbl.py
@@ -1,6 +1,10 @@
 from discord.ext import commands
 import discord
 from discord.ext.commands import bot
+from .crawler.crawler import get_cpbl_now
+
+import datetime
+import pytz
 
 
 class Cpbl(commands.Cog):
@@ -8,8 +12,21 @@ class Cpbl(commands.Cog):
         self.bot = bot
     
     @commands.command()
-    async def cpbl(self, ctx):
-        await ctx.send("中華職棒")
+    async def cpbl_now(self, ctx):
+        taipei_timezone = pytz.timezone('Asia/Taipei')
+        taipei_time = datetime.datetime.now(taipei_timezone)
+        game_status_dict = get_cpbl_now()
+        embed=discord.Embed(title="中職比分", description=str(taipei_time.strftime("%Y-%m-%d %H:%M:%S ")),url="http://cpbl.com.tw/",color=0x4b83dd)
+        if game_status_dict['game_end']:
+            for game in game_status_dict['game_end']:
+                embed.add_field(name=game, value="比賽結束", inline=False)
+        if game_status_dict['game_wait']:
+            for game in game_status_dict['game_wait']:
+                embed.add_field(name=game, value="比賽未開始", inline=False) 
+        if game_status_dict['game_live']:
+            for game in game_status_dict['game_live']:
+                embed.add_field(name=game, value="比賽進行中", inline=False)
+        await ctx.send(embed=embed)
     
 async def setup(bot):
     await bot.add_cog(Cpbl(bot))
--- main.py
+++ main.py
@@ -7,13 +7,33 @@ from Cog import cpbl
 from keep_alive import keep_alive
 
 DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
-OWNER_ID = os.environ['DISCORD_TOKEN']
+OWNER_ID = os.environ['OWNER_ID']
 
-keep_alive()
-#client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
 bot = commands.Bot(command_prefix = '!', 
                    owner_ids = OWNER_ID,
                    intents = discord.Intents.all())
+
+@bot.command()
+async def load(ctx, extension):
+    await bot.load_extension(f"Cog.{extension}")
+    await ctx.send(f"Loaded {extension} done.")
+
+# 卸載指令檔案
+@bot.command()
+async def unload(ctx, extension):
+    await bot.unload_extension(f"Cog.{extension}")
+    await ctx.send(f"UnLoaded {extension} done.")
+
+# 重新載入程式檔案
+@bot.command()
+async def reload(ctx, extension):
+    await bot.reload_extension(f"Cog.{extension}")
+    await ctx.send(f"ReLoaded {extension} done.")
+
+
+
+keep_alive()
+
 
 
 async def main():
@@ -23,3 +43,4 @@ async def main():
         await bot.start(DISCORD_TOKEN)
 
 asyncio.run(main())
+#client.run(main())
